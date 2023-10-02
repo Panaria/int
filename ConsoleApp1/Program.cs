d9feb39abda2401a63668811caee324d30bf1d15
Panaria <baranyldzby@gmail.com>
2023-10-02 21:35:27 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h;
+            h = 6;
+            
+            Console.WriteLine(h + 1);
+
+            Console.ReadLine();
+        }
+    }
+}
